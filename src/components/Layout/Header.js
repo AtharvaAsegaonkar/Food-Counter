c023c5cba95a1706d64ddaec0fc794912fba869a
Atharva Asegaonkar <108936127+AtharvaAsegaonkar@users.noreply.github.com>
2022-09-28 21:56:01 +0530

    Added some Feature

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Too-Hungry</h1>
+        <h1>FoodiZone</h1>
         <HeaderCartButton  onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
